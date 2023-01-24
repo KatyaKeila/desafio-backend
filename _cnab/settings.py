5470183419c7dccc942325a7beaf1947e123398d
Kátya <katyakeila@gmail.com>
2023-01-24 06:25:20 -0300

    database connection

--- _cnab/settings.py
+++ _cnab/settings.py
@@ -78,10 +78,18 @@ WSGI_APPLICATION = "_cnab.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+	"default": {
+	"ENGINE": "django.db.backends.postgresql",
+	"NAME": os.getenv("POSTGRES_DB_NAME"),
+	"USER": os.getenv("POSTGRES_USERNAME"),
+	"PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+	"HOST": "127.0.0.1",
+	"PORT": "5432",
+},
+"outro_nome": {
+	"ENGINE": "django.db.backends.sqlite3",
+	"NAME": BASE_DIR / "db.sqlite3",
+	},
 }
 
 
