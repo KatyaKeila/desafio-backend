7e435cbec7fe676017d1ed398aa12091bdf585b1
Kátya <katyakeila@gmail.com>
2023-02-07 15:22:39 -0300

    create views

--- form/views.py
+++ form/views.py
@@ -1,17 +1,13 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from rest_framework.views import APIView
-
 from .forms import UploadFileForm
 
 
-def form_modelform(request):
+"""def form_modelform(request):
     if request.method == "GET":
         form = UploadFileForm()
-        context = {
-            "form": form,
-        }
 
-        return render(request, "form/form_modelform.html", context=context)
+        return render(request, "form/form_modelform.html", {"form": form})
 
     else:
         form = UploadFileForm(request.POST)
@@ -19,8 +15,16 @@ def form_modelform(request):
             upload = form.save()
             form = UploadFileForm()
 
-        context = {
-            "form": form,
-        }
+        return render(request, "form/form_modelform.html", {"form": form})"""
 
-        return render(request, "form/form_modelform.html", context=context)
+
+def form_modelform(request):
+    if request.method == "POST":
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("form/form_modelform.html")
+
+    else:
+        form = UploadFileForm()
+    return render(request, "form/form_modelform.html", {"form": form})
