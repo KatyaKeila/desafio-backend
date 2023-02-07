abd37f65e45e3b0a811c5a8cf58f6c2a7fc0e1c1
Kátya <katyakeila@gmail.com>
2023-02-07 16:10:41 -0300

    update views

--- form/views.py
+++ form/views.py
@@ -3,21 +3,6 @@ from django.shortcuts import render
 from .forms import UploadFileForm
 
 
-"""def form_modelform(request):
-    if request.method == "GET":
-        form = UploadFileForm()
-
-        return render(request, "form/form_modelform.html", {"form": form})
-
-    else:
-        form = UploadFileForm(request.POST)
-        if form.is_valid():
-            upload = form.save()
-            form = UploadFileForm()
-
-        return render(request, "form/form_modelform.html", {"form": form})"""
-
-
 def form_modelform(request):
     if request.method == "POST":
         form = UploadFileForm(request.POST, request.FILES)
